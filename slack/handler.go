5ceb9de1d04ffb6b9ee1f3b25e6ca16e39d0ade2
Alex Ellis <alexellis2@gmail.com>
2018-10-07 18:34:38 +0300

    Split functions
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- slack/handler.go
+++ slack/handler.go
@@ -7,12 +7,12 @@ import (
 
 // Handle a request with your middleware
 func Handle(w http.ResponseWriter, r *http.Request) {
-	if urlVal, ok := os.LookupEnv("gist_url"); ok && len(urlVal) > 0 {
+	if urlVal, ok := os.LookupEnv("slack_url"); ok && len(urlVal) > 0 {
 
 		http.Redirect(w, r, urlVal, http.StatusPermanentRedirect)
 
 	} else {
-		http.Error(w, "Unable to find gist_url variable", http.StatusInternalServerError)
+		http.Error(w, "Unable to find slack_url variable", http.StatusInternalServerError)
 
 	}
 }
