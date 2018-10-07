be571dd1fa9d9c9770dd6c8b43a410283ebdd4c1
Alex Ellis <alexellis2@gmail.com>
2018-10-07 18:40:36 +0300

    JS redirect
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- slack/handler.go
+++ slack/handler.go
@@ -9,7 +9,19 @@ import (
 func Handle(w http.ResponseWriter, r *http.Request) {
 	if urlVal, ok := os.LookupEnv("slack_url"); ok && len(urlVal) > 0 {
 
-		http.Redirect(w, r, urlVal, http.StatusPermanentRedirect)
+		w.Write([]byte(`<html>
+<head>
+</head>
+<body>
+		<a href="` + urlVal + `">Redirecting.. click here.</a>
+</body>
+<script>
+	setTimeout(function() {
+		document.href = "` + urlVal + `";
+	}, 100);
+</script
+</html>
+)`))
 
 	} else {
 		http.Error(w, "Unable to find slack_url variable", http.StatusInternalServerError)
