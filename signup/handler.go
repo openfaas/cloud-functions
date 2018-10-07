5431176734a51ff87cd5c37a1519561e2e6f1796
Alex Ellis <alexellis2@gmail.com>
2018-10-07 18:32:21 +0300

    Error handling for empty URL
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- signup/handler.go
+++ signup/handler.go
@@ -7,12 +7,11 @@ import (
 
 // Handle a request with your middleware
 func Handle(w http.ResponseWriter, r *http.Request) {
-	if urlVal, ok := os.LookupEnv("webpage_url"); ok {
+	if urlVal, ok := os.LookupEnv("webpage_url"); ok && len(urlVal) > 0 {
 
 		http.Redirect(w, r, urlVal, http.StatusPermanentRedirect)
 
 	} else {
-
 		http.Error(w, "Unable to find webpage_url variable", http.StatusInternalServerError)
 
 	}
